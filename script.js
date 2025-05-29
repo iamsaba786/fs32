ed79cf2d5d91ebb78338630a354878e99f990ae7
iamsaba786 <snaz7571@gmail.com>
2025-05-29 17:24:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// object
+// what is object ?
+// how to create obj
+// how to access obj value.
+// what is dom ?
+let arr = [];
+for (let i = 1; i < 6; i++) {
+  a = prompt("enter number");
+  a = Number(a.trim());
+  arr.push(a);
+}
+console.log(arr);
+//how to i host / deploye my projects.
+//github par. youtube
+//trainning llm models
